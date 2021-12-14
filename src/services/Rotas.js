e0f61ee6c83d0c770f0471d10f12145bd2c4bdc9
Vitoria <vick_santos_vick@hotmail.com>
2021-12-14 16:20:15 -0300

    services

---
+++ src/services/Rotas.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Route, BrowserRouter } from "react-router-dom";
+
+import StartPage from "../Pages/StartPage";
+import Login from "../Pages/Login";
+import CadastrarCliente from "../Pages/CadastrarCliente";
+import HomePageFuncionarios from "../Pages/HomePageFuncionarios";
+import CadFuncionarios from "../Pages/CadFuncionarios";
+import CadVeiculos from "../Pages/CadVeiculos";
+import Reservas from "../Pages/Reservas";
+import HomePageClientes from "../Pages/HomePageClientes";
+import CadFornecedores from "../Pages/CadFornecedores";
+import Funcionarios from "../Pages/Funcionarios"
+import VeiculosF from "../Pages/VeiculosF"
+import Fornecedores from "../Pages/Fornecedores";
+
+const Rotas = () => {
+   return(
+       <BrowserRouter>
+           <Route component = { StartPage }  path="/" exact />
+           <Route component = { Login }  path="/login" />
+           <Route component = { CadastrarCliente }  path="/cadcliente" />
+           <Route component = { HomePageFuncionarios }  path="/homef" />
+           <Route component = { CadFornecedores }  path="/cadfornecedores" />
+           <Route component = { CadFuncionarios }  path="/cadfuncionarios" />
+           <Route component = { CadVeiculos }  path="/cadveiculos" />
+           <Route component = { Reservas }  path="/reservas" />
+           <Route component = { HomePageClientes }  path="/homec" />
+           <Route component = { Funcionarios }  path="/funcionarios" />
+           <Route component = { VeiculosF }  path="/veiculosf" />
+           <Route component = { Fornecedores }  path="/fornecedores" />
+
+       </BrowserRouter>
+   )
+}
+
+export default Rotas;
---
+++ src/services/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCdN1WsH7agQb0ZZm4rAhThy32-HmY0x2w",
+  authDomain: "pweb-69be9.firebaseapp.com",
+  projectId: "pweb-69be9",
+  storageBucket: "pweb-69be9.appspot.com",
+  messagingSenderId: "517444776352",
+  appId: "1:517444776352:web:0356289957dd82d10974c5",
+  measurementId: "G-GYRLEPEQZ6"
+};
+
