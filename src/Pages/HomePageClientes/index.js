dda05ae773371ddf779910542b4eeb46eef95e11
Vitoria <vick_santos_vick@hotmail.com>
2021-12-14 16:18:58 -0300

    Home Page Cliente

---
+++ src/Pages/HomePageClientes/index.js
@@ -0,0 +1,10 @@
+import NavBarClientes from '../componentes/NavBarClientes';
+import './hmclientes-styles.css';
+
+export default function HomePageClientes(){
+    return (
+        <div className='container-homec'>
+            <NavBarClientes></NavBarClientes>
+        </div>
+    );
+}
