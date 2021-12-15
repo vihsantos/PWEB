df8d867d3edf9e9ad2aef606b682a810e2f01631
Vitoria <vick_santos_vick@hotmail.com>
2021-12-15 02:12:38 -0300

    funcionarios conexão com o bd

--- src/Pages/Funcionarios/index.js
+++ src/Pages/Funcionarios/index.js
@@ -2,27 +2,55 @@ import './fun-styles.css';
 import Person from "/pweb/src/assets/person.png"
 import NavBarFuncionarios from '../componentes/NavBarFuncionarios';
 import IconAdd from "/pweb/src/assets/icon-add.png"
+import { Link } from 'react-router-dom';
+import React from 'react';
+import { useEffect } from 'react';
+import {collection, getDocs} from "firebase/firestore";
+import { db } from 'services/firebase';
+import { useState } from 'react';
+
 
 export default function Funcionarios(){
+    const [funcionarios, setFuncionarios] = useState([]);
+    const funcionariosCollectionsRef = collection(db, "funcionarios");
+    useEffect(()=>{
+
+        const getFuncionarios = async () =>{
+            const data = await getDocs(funcionariosCollectionsRef);
+            setFuncionarios(data.docs.map((doc) => ({
+                ...doc.data(), id: doc.id
+            })))
+        }
+        getFuncionarios()
+    }, [])
+
     return (
         <div className='container-fun'>
             <NavBarFuncionarios></NavBarFuncionarios>
             <div className='content-fun'>
-                <div className='card-funcionarios'>
-                    <div className='circle-person'>
-                        <img className='person-img' src={Person} alt='pessoa'></img>
-                    </div>
-                    <div className='campos-fun'>
-                        <div className='label-fun'>Nome:</div>
-                        <div className='valor-fun'>Qualquer</div>
-                    </div>
-                    <div className='campos-fun'>
-                        <div className='label-fun'>Nome:</div>
-                        <div className='valor-fun'>Qualquer</div>
-                    </div>
-                </div>
+                {
+                    funcionarios.map((funcionario)=>{
+                        return(
+                            <div className='card-funcionarios' id={funcionario.id}>
+                                <div className='circle-person'>
+                                    <img className='person-img' src={Person} alt='pessoa'></img>
+                                </div>
+                                <div className='campos-fun'>
+                                    <div className='label-fun'>Nome:</div>
+                                    <div className='valor-fun'>{funcionario.nome}</div>
+                                </div>
+                                <div className='campos-fun'>
+                                    <div className='label-fun'>Privilégios de acesso:</div>
+                                    <div className='valor-fun'>{funcionario.privacessos}</div>
+                                </div>
+                            </div>
+                        );
+                    })
+                }
                 <div className='adicionar-funcionarios'>
-                    <img className='add-fun' src={IconAdd} alt='add-fun'></img>
+                    <Link to="/cadfuncionarios">
+                        <img className='add-fun' src={IconAdd} alt='add-fun'></img>
+                    </Link>
                 </div>
             </div>
         </div>
