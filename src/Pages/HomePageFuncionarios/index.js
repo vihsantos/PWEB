e3ebd5ffc1b6f42ab2d206c84ddbf791a0d45bec
Vitoria <vick_santos_vick@hotmail.com>
2021-12-15 16:48:23 -0300

    home funcionarios

--- src/Pages/HomePageFuncionarios/index.js
+++ src/Pages/HomePageFuncionarios/index.js
@@ -1,18 +1,14 @@
 import "./hm_funcionarios.css";
 import NavBarFuncionarios from "../componentes/NavBarFuncionarios"
+import React from "react";
 
 export default function HomePageFuncionarios(){
+
     return(
         <div className="container-hmfuncionarios">
             <NavBarFuncionarios></NavBarFuncionarios>
             <div className="content-hmf">
-                <div className="quant-veiculos">
-                    <div className="valor">36</div>
-                    <div className="subtitulo-valor">Veiculos Reservados</div>
-                </div>
-                <div className="chart">
-                    
-                </div>
+                
             </div>
         </div>
         
