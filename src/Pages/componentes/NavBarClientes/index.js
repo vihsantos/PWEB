fb4f59f677fb41e2d6771b9909fb39c0361ded5f
Vitoria <vick_santos_vick@hotmail.com>
2021-12-15 16:48:00 -0300

    editando o navbarcliente

--- src/Pages/componentes/NavBarClientes/index.js
+++ src/Pages/componentes/NavBarClientes/index.js
@@ -1,6 +1,7 @@
 import "./nc-styles.css";
 import { Link } from 'react-router-dom';
 import BotaoSair from "/pweb/src/assets/botaosair.png"
+import React from "react";
 
 
 export default function NavBarClientes(){
@@ -11,8 +12,7 @@ export default function NavBarClientes(){
             </h1>
             <ul>
                     <li><Link to="/homec">Home</Link></li>
-                    <li><Link to="/veiculosc">Veículos</Link></li>
-                    <li><Link to="">Reservar</Link></li>
+                    <li><Link to="/reservar">Reservar</Link></li>
                     <li>
                         <div className="sair" >
                             <img className="botao-sair" src={BotaoSair} alt="sair"></img>
