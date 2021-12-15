d5c0086e38cc0795a7ba2b110a9bd39b79d87a57
Vitoria <vick_santos_vick@hotmail.com>
2021-12-15 16:48:13 -0300

    home clientes

--- src/Pages/HomePageClientes/index.js
+++ src/Pages/HomePageClientes/index.js
@@ -1,10 +1,22 @@
+import React, { useState } from 'react';
 import NavBarClientes from '../componentes/NavBarClientes';
 import './hmclientes-styles.css';
 
 export default function HomePageClientes(){
+
+    const [veiculosNaoReservados, setVeiculosNaoReservados] = useState([]);
+
     return (
         <div className='container-homec'>
             <NavBarClientes></NavBarClientes>
+            <div className='content-homec'>
+                <div className='cardveiculo-cliente'>
+                    <img src={"veiculo.imagem"} className='imagem-veic' alt='imagem-veic'></img>
+                    <div className='label-veic'>veiculo.nome</div>
+                    <div className='label-veic'>veiculo.marca</div>
+                    <div className='label-veic'>veiculo.placa</div>
+                </div>
+            </div>
         </div>
     );
 }
