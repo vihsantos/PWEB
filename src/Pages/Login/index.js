17c6e04000df69c006c128b076c35b20efcb21fa
Vitoria <vick_santos_vick@hotmail.com>
2021-12-15 16:49:22 -0300

    login quase funcionando

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,8 +1,26 @@
 import "./styles-login.css";
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { auth } from "services/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 
 export default function Login(){
+
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    
+    
+    const login = async () => {
+        try {
+            const user = await signInWithEmailAndPassword(auth, email, senha);
+            
+            console.log(user);
+
+        } catch(error){
+            console.log(error.message);
+        }
+    }
+
     return(
         <main>
             <div className="container-login">
@@ -11,11 +29,11 @@ export default function Login(){
                 </div>
                 <div className="text-field">
                     <label htmlFor="email">Email:</label>
-                    <input type="text" placeholder="Digite o seu email..." ></input>
+                    <input type="text" placeholder="Digite o seu email..." onChange={(event)=>{setEmail(event.target.value)}}></input>
                 </div>
                 <div className="text-field">
                     <label htmlFor="senha">Senha:</label>
-                    <input type="text" placeholder="Digite a sua senha..."></input>
+                    <input type="text" placeholder="Digite a sua senha..." onChange={(event)=>{setSenha(event.target.value)}}></input>
                 </div>
                 <button className="botao-entrar" >
                     Entrar
