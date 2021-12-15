12e90ef37fba8ceb8744da190acc035fdbb8bd48
Vitoria <vick_santos_vick@hotmail.com>
2021-12-15 16:49:08 -0300

    reservar cliente

---
+++ src/Pages/Reservar/index.js
@@ -0,0 +1,10 @@
+import NavBarClientes from "../componentes/NavBarClientes";
+import "./reservar-styles.css";
+
+export default function Reservar(){
+    return(
+        <div className="container-reservar">
+            <NavBarClientes></NavBarClientes>
+        </div>
+    );
+}
