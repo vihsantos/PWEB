626d75052e1b58aa894ae8e6d938e260887014bf
Vitoria <vick_santos_vick@hotmail.com>
2021-12-16 01:29:11 -0300

    paginas de reservas funcionarios

--- src/Pages/Reservas/index.js
+++ src/Pages/Reservas/index.js
@@ -1,26 +1,48 @@
 import "./reservas-styles.css"
 import NavBarFuncionarios from "../componentes/NavBarFuncionarios"
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "configuracoes/Firebase";
 
 export default function Reservas(){
+    const [reservasf, setReservasf] = useState([]);
+    const reservasfCollectionsRef = collection(db, "reservasf");
+    useEffect(()=>{
+
+        const getreservasf = async () =>{
+            const data = await getDocs(reservasfCollectionsRef);
+            setReservasf(data.docs.map((doc) => ({
+                ...doc.data(), id: doc.id
+            })))
+            console.log(data);
+        }
+        getreservasf()
+    }, )
+
     return (
         <div className="container-reservasf">
             <NavBarFuncionarios></NavBarFuncionarios>
             <div className="content-reservasf">
-                <div className="card-reservasf">
-                    <div className="campo-reservasf">
-                        <div className="label-reservasf">Cliente:</div>
-                        <div className="valor-reservasf"> Qualquer</div>
-                    </div>
-                    <div className="campo-reservasf">
-                        <div className="label-reservasf">Veiculo:</div>
-                        <div className="valor-reservasf"> Qualquer</div>
-                    </div>
-                    <div className="campo-reservasf">
-                        <div className="label-reservasf">Aprovacão:</div>
-                        <div className="valor-reservasf"> Qualquer</div>
-                    </div>
-                </div>
+                {
+                    reservasf.map((reservaf)  =>{
+                        return(
+                            <div className="card-reservasf" id={reservaf.id}>
+                                <div className="campo-reservasf">
+                                    <div className="label-reservasf">Cliente:</div>
+                                    <div className="valor-reservasf"> {reservaf.nomeCliente}</div>
+                                </div>
+                                <div className="campo-reservasf">
+                                    <div className="label-reservasf">Veiculo:</div>
+                                    <div className="valor-reservasf"> {reservaf.veiculo}</div>
+                                </div>
+                                <div className="campo-reservasf">
+                                    <div className="label-reservasf">Estado:</div>
+                                    <div className="valor-reservasf"> {reservaf.estado}</div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     );
